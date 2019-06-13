2adc0e9774f9af28bb5a93720f9ea1b123b32a0a
rkabrick <rkabrick@udel.edu>
2019-06-13 13:05:20 -0500

    Lazy Eval/Memoization

--- irpc.py
+++ irpc.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys, copy
-from pycparser.c_ast import FuncCall, ID, Decl
+from pycparser.c_ast import *
+#FuncCall, ID, Decl, TypeDecl, IdentifierType, If
 
 from irpc.irpctyping import *
 from irpc.bindingEntity import entity2Compound
@@ -26,7 +27,7 @@ def provider_name(provdef: ProvDef):
 
 def hoist_declaration(main: FuncDef,
                       provdef: ProvDef):
-    
+
     entname = provider_name(provdef)
     l_node = provdef.body.block_items
     for i, node in enumerate(l_node):
@@ -35,17 +36,31 @@ def hoist_declaration(main: FuncDef,
                     main.insert(0, node)
                     break
 
+def hoist_prov_memo(main: FuncDef,
+                    provdef: ProvDef):
+
+    _name_ = f"{e}_provided"
+    memo_node = Decl(name=_name_,
+        quals=[], storage=[],funcspec=[],
+        type=TypeDecl(declname=_name_, quals=[],
+                    type=IdentifierType(names=['bool'])),
+        init=ID(name='False'),
+        bitsize=None
+        )
+    main.insert(1, memo_node)
+
 def add_provider_call(funcdef: FuncDef,
                       entnames: Set[Entity]):
 
     if is_provider(funcdef):
         entnames = entnames - set([provider_name(funcdef)])
-                
+
     # Insert the provider call
     for e, l_compound in entity2Compound(funcdef.body, entnames).items():
           for compound in l_compound:
-                f = FuncCall(name=ID(name=f'provide_{e}'), args=None)
-                compound.block_items.insert(0, f)
+                prov_if = If(cond=UnaryOp(op='!', expr=ID(name=f'{e}_provided')), iftrue=Compound(block_items=(FuncCall(name=ID(name=f'provide_{e}'), args=None), Assignment(op='=',lvalue=ID(name=f'{e}_provided'), rvalue=Constant(type='bool', value='true')))), iffalse=None)
+                compound.block_items.insert(0, prov_if)
+                
 
 if __name__ == "__main__":
 
@@ -64,8 +79,14 @@ if __name__ == "__main__":
     for f in l_func:
         add_provider_call(f, l_ent)
 
-    for e in l_provider:
-        hoist_declaration(ast.ext, e)
+        
 
+    for p in l_provider:
+        hoist_declaration(ast.ext, p)
+
+    for e in l_ent:
+        hoist_prov_memo(ast.ext, e)
+
+    print(ast.ext) 
     generator = c_generator.CGenerator()
     print(generator.visit(ast))
