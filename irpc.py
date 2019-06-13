7ff9efe38d2e48a321457198034cae1f61223eac
rkabrick <rkabrick@udel.edu>
2019-06-13 15:31:23 -0500

    Lazy Eval/Memoization w/ Resolved Requests

--- irpc.py
+++ irpc.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, copy
-from pycparser.c_ast import *
-#FuncCall, ID, Decl, TypeDecl, IdentifierType, If
+from pycparser.c_ast import FuncCall, ID, Decl, TypeDecl, IdentifierType, If, UnaryOp, Compound, Assignment, Constant
 
 from irpc.irpctyping import *
 from irpc.bindingEntity import entity2Compound
@@ -27,39 +26,41 @@ def provider_name(provdef: ProvDef):
 
 def hoist_declaration(main: FuncDef,
                       provdef: ProvDef):
-
+    
     entname = provider_name(provdef)
     l_node = provdef.body.block_items
-    for i, node in enumerate(l_node):
-             if isinstance(node,Decl) and node.type.declname == entname:
-                    node = l_node.pop(i)
-                    main.insert(0, node)
-                    break
-
-def hoist_prov_memo(main: FuncDef,
-                    provdef: ProvDef):
-
-    _name_ = f"{e}_provided"
+    _name_ = f'{entname}_provided'
+    _type_ = TypeDecl(declname=_name_, quals=[], type=IdentifierType(names=['bool']))
+    _init_ = init=ID(name='False')
+    
     memo_node = Decl(name=_name_,
-        quals=[], storage=[],funcspec=[],
-        type=TypeDecl(declname=_name_, quals=[],
-                    type=IdentifierType(names=['bool'])),
-        init=ID(name='False'),
-        bitsize=None
-        )
-    main.insert(1, memo_node)
-
+                     quals=[],
+                     storage=[],
+                     funcspec=[],
+                     type= _type_,
+                     init=_init_,
+                     bitsize=None)
+    
+    for i, node in enumerate(l_node):
+        if isinstance(node,Decl) and node.type.declname == entname:
+            node = l_node.pop(i)
+            main.insert(0, node)
+            main.insert(1, memo_node)
+            break
+        
 def add_provider_call(funcdef: FuncDef,
                       entnames: Set[Entity]):
-
+            
     if is_provider(funcdef):
         entnames = entnames - set([provider_name(funcdef)])
-
+                
     # Insert the provider call
     for e, l_compound in entity2Compound(funcdef.body, entnames).items():
-          for compound in l_compound:
-                prov_if = If(cond=UnaryOp(op='!', expr=ID(name=f'{e}_provided')), iftrue=Compound(block_items=(FuncCall(name=ID(name=f'provide_{e}'), args=None), Assignment(op='=',lvalue=ID(name=f'{e}_provided'), rvalue=Constant(type='bool', value='true')))), iffalse=None)
-                compound.block_items.insert(0, prov_if)
+        for compound in l_compound:
+            provider = f'provide_{e}'
+            provider_bool = f'{e}_provided'
+            prov_if = If(cond=UnaryOp(op='!', expr=ID(name=provider_bool)), iftrue=Compound(block_items=(FuncCall(name=ID(name=provider), args=None), Assignment(op='=',lvalue=ID(name=provider_bool), rvalue=Constant(type='bool', value='true')))), iffalse=None)
+            compound.block_items.insert(0, prov_if)
                 
 
 if __name__ == "__main__":
@@ -71,22 +72,16 @@ if __name__ == "__main__":
                      use_cpp=True,
                      cpp_path='gcc',
                      cpp_args=['-E'])
-
+    
     l_func = { f for f in ast.ext if isinstance(f, FuncDef) }
     l_provider = { f for f in l_func if is_provider(f) }
     l_ent  = { provider_name(e) for e in l_provider }
-
+    
     for f in l_func:
         add_provider_call(f, l_ent)
-
         
-
     for p in l_provider:
         hoist_declaration(ast.ext, p)
 
-    for e in l_ent:
-        hoist_prov_memo(ast.ext, e)
-
-    print(ast.ext) 
     generator = c_generator.CGenerator()
     print(generator.visit(ast))
