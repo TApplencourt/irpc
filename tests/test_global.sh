bc7c48d3b5b4844b2f259621c4da8b3af29505c7
TApplencourt <tapplencourt@gmail.com>
2019-06-18 22:14:04 -0500

    Update test

--- tests/test_global.sh
+++ tests/test_global.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-cd ..
-
 # Output checking
 ./irpc.py ./example/simple.irp.c > tests/cur_test.c;
 diff ./example/gold.simple.c tests/cur_test.c;
 rm -f tests/cur_test.c
-
-#Python unittest
-# Will run all the test*.py
-python -m unittest discover
