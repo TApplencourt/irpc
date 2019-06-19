7d3c00f364e9d41225db132180707cdd616e5bfe
rkabrick <rkabrick@udel.edu>
2019-06-18 22:10:43 -0500

    intro to mutability/Formatting Improvements

--- irpc.py
+++ irpc.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import sys, copy
-from pycparser.c_ast import FuncCall, ID, Decl, TypeDecl, IdentifierType, If, UnaryOp, Compound, Assignment, Constant
+
+from collections import defaultdict
+
+from pycparser import c_ast
+from pycparser.c_ast import FuncCall, ID, Decl, TypeDecl, FuncDecl, IdentifierType, If, UnaryOp, Compound, Assignment, Constant
 
 from irpc.irpctyping import *
 from irpc.bindingEntity import entity2Compound
@@ -18,29 +22,51 @@ from irpc.bindingEntity import entity2Compound
 # 2/ Inside function body, insert a call to the provider before any usage of an entity.
 # 3/ Inside provider body, hoist the entity declaration
 
+# A visitor with some state information (the funcname it's looking for)
+
 def is_provider(funcdef: FuncDef):
     return funcdef.decl.name.startswith('provide_')
 
 def provider_name(provdef: ProvDef):
     return provdef.decl.name.split('provide_').pop()
 
+def touch_name(entity):
+    return f'touch_{entity}'
+
 def entity_memo_flag(entity):
     return f'{entity}_provided'
 
-def hoist_declaration(main: FuncDef,
+ # Generate "f('bool {entname} = False') 
+def ast_entity_memo_flag(entname):
+    entity_flag = entity_memo_flag(entname) 
+    type_ = TypeDecl(declname = entity_flag,
+                     quals=[], type=IdentifierType(names=['bool']))
+    return Decl(name=entity_flag, quals=[],
+                storage=[], funcspec=[],
+                type= type_, init= ID(name='False'),
+                bitsize=None)
+
+def touch_declaration(main: FuncDef,
                       provdef: ProvDef):
+    touch_name_decl = touch_name(entname)
+    type_ = FuncDecl(args=None, type=TypeDecl(declname = touch_name_decl,
+                                              quals=[], type=IdentifierType(names=['void'])))
+    return Decl(name=touch_name_decl,
+                quals=[], storage=[], funcspec=[],
+                type= type_,
+                init= None,
+                bitsize=None)
 
-    # Generate "f('bool {entname} = False') 
-    def ast_entity_memo_flag(entname):
-        entity_flag = entity_memo_flag(entname)
-        type_ = TypeDecl(declname = entity_flag,
-                         quals=[], type=IdentifierType(names=['bool']))
-        return Decl(name=entity_flag, quals=[],
-                    storage=[], funcspec=[],
-                    type= type_, init= ID(name='False'),
-                    bitsize=None)
+def touch_definition(entname):
+    touch_name_decl = touch_name(entname)
+    type_ = FuncDecl(args=None, type=TypeDecl(declname = touch_name_decl,
+                                              quals=[], type=IdentifierType(names=['void'])))
+    return FuncDef(decl=Decl(name=touch_name_decl, quals=[],storage=[],funcspec=[],type=type_, init=None, bitsize=None), param_decls=None, body=Compound(block_items=[]))
 
 
+def hoist_declaration(main: FuncDef,
+                      provdef: ProvDef):       
+        
     entname = provider_name(provdef)
     l_node = provdef.body.block_items
     # Move the declaration of the entity and the top of the file
@@ -48,8 +74,27 @@ def hoist_declaration(main: FuncDef,
     for i, node in enumerate(l_node):
         if isinstance(node,Decl) and node.type.declname == entname:
             node = l_node.pop(i)
+
+            # Insert Declaration 
             main.insert(0, node)
-            main.insert(1, ast_entity_memo_flag(entname) )
+
+            # Insert Memo Flag
+            main.insert(1, ast_entity_memo_flag(entname))
+
+            # Setup and Insert Touch_*() function
+            new = touch_definition(entname)
+
+            # Insert Memo Flag = True for respective entity's touch function
+            self_touch_element = ast_entity_memo_flag(entname)
+            self_touch_element.init.name='True'
+            new.body.block_items.insert(0, self_touch_element)
+
+            # Add false flags to all adjacent nodes in the upward graph
+            for k,v in upward_graph.items():
+                if (entname == k):
+                    for value in v:
+                        new.body.block_items.insert(0, ast_entity_memo_flag(value))
+            main.insert(2,new)
             break
 
 def add_provider_call(funcdef: FuncDef,
@@ -64,16 +109,21 @@ def add_provider_call(funcdef: FuncDef,
                                                                  lvalue=ID(name=entity_flag),
                                                                  rvalue=Constant(type='bool', value='True'))]),
                                                       iffalse=None)
-             
 
     if is_provider(funcdef):
         entnames = entnames - set([provider_name(funcdef)])
 
     # Insert the provider call
     for e, l_compound in entity2Compound(funcdef.body, entnames).items():
-          provider_call = ast_cached_provider_call(e)
-          for compound in l_compound:
-              compound.block_items.insert(0, provider_call)
+        provider_call = ast_cached_provider_call(e)
+        for compound in l_compound:
+            if is_provider(funcdef):
+                prov_name = funcdef.decl.name.split('provide_').pop()
+                downward_graph[prov_name].add(e)
+                upward_graph[e].add(prov_name)
+                compound.block_items.insert(0, provider_call)
+
+    
 
 if __name__ == "__main__":
 
@@ -85,15 +135,35 @@ if __name__ == "__main__":
                      cpp_path='gcc',
                      cpp_args=['-E'])
 
+    downward_graph = defaultdict(set)
+    upward_graph = defaultdict(set)
+
     l_func = { f for f in ast.ext if isinstance(f, FuncDef) }
     l_provider = { f for f in l_func if is_provider(f) }
     l_ent  = { provider_name(e) for e in l_provider }
 
     for f in l_func:
-        add_provider_call(f, l_ent)
-
+            add_provider_call(f, l_ent)
+        
     for p in l_provider:
         hoist_declaration(ast.ext, p)
 
+    #for key, value in upward_graph.items():
+    #print(key,value)
+
+    
+       
+
+    #for t in l_touch:
+    #    print(l_touch)
+        #hoist_touch_declaration(ast.ext, t)
+    #    add_touch_call(t, l_ent)
+
+    #print(l_touch)
+
+    #print(l_provider)
+
+    #print(l_func)
     generator = c_generator.CGenerator()
+    
     print(generator.visit(ast))
