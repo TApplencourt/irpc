560ed39edae5228cd97e3cd3bb04462f3914958d
rkabrick <rkabrick@udel.edu>
2019-07-03 18:34:02 -0500

    Updated tests which don't account for whitespace

--- tests/test_global.sh
+++ tests/test_global.sh
@@ -2,14 +2,14 @@
 
 # Output checking
 ./irpc.py ./examples/simple.irp.c > tests/simple.c
-diff ./tests/gold.simple.c tests/simple.c
+diff -w ./tests/gold.simple.c tests/simple.c
 
 ./irpc.py ./examples/touch.irp.c > tests/touch.c
-diff ./tests/gold.touch.c tests/touch.c
+diff -w ./tests/gold.touch.c tests/touch.c
 
 # Compiling to be sure
 gcc tests/simple.c -o tests/simple.exe
 ./tests/simple.exe > tests/simple.out
 
 # Output checking
-diff ./tests/gold.simple.out tests/simple.out
+diff -w ./tests/gold.simple.out tests/simple.out
