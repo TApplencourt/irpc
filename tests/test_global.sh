29f8e819bc431191396c9631067c84adc146ab68
rkabrick <31963805+rkabrick@users.noreply.github.com>
2019-07-08 12:09:34 -0500

    Getting rid of -I flag in test
    -I broke it more than it was before

--- tests/test_global.sh
+++ tests/test_global.sh
@@ -2,14 +2,14 @@
 
 # Output checking
 ./irpc.py ./examples/simple.irp.c > tests/simple.c
-diff -w -b -B -I ./tests/gold.simple.c tests/simple.c
+diff -wbB ./tests/gold.simple.c tests/simple.c
 
 ./irpc.py ./examples/touch.irp.c > tests/touch.c
-diff -w -b -B -I ./tests/gold.touch.c tests/touch.c
+diff -wbB ./tests/gold.touch.c tests/touch.c
 
 # Compiling to be sure
 gcc tests/simple.c -o tests/simple.exe
 ./tests/simple.exe > tests/simple.out
 
 # Output checking
-diff -w -b -B -I ./tests/gold.simple.out tests/simple.out
+diff -wbB ./tests/gold.simple.out tests/simple.out
