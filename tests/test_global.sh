9540d39ee1a355530aeea6f18e30ca42fd62b52c
rkabrick <31963805+rkabrick@users.noreply.github.com>
2019-07-08 12:05:13 -0500

    Trying diff with another flag
    -I will hopefully ignore lines that are the same. This is important because the work log on Travis shows the output of the diff being the same lines

--- tests/test_global.sh
+++ tests/test_global.sh
@@ -2,14 +2,14 @@
 
 # Output checking
 ./irpc.py ./examples/simple.irp.c > tests/simple.c
-diff -w -b -B ./tests/gold.simple.c tests/simple.c
+diff -w -b -B -I ./tests/gold.simple.c tests/simple.c
 
 ./irpc.py ./examples/touch.irp.c > tests/touch.c
-diff -w -b -B ./tests/gold.touch.c tests/touch.c
+diff -w -b -B -I ./tests/gold.touch.c tests/touch.c
 
 # Compiling to be sure
 gcc tests/simple.c -o tests/simple.exe
 ./tests/simple.exe > tests/simple.out
 
 # Output checking
-diff -w -b -B ./tests/gold.simple.out tests/simple.out
+diff -w -b -B -I ./tests/gold.simple.out tests/simple.out
